a37dda451cbb63d97823c520897683f82af54d0a
sean-code <john.nganga@student.moringaschool.com>
2022-08-26 14:02:02 +0300

    Lab Done

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 function isPalindrome(word) {
   // Write your algorithm here
+// word.split('').reverse().join();
+
+const letters = word.split('');
+const reverseLetters = letters.reverse();
+const reverseString  = reverseLetters.join('')
+
+if (word === reverseString){
+  return true
+
+} else {
+  return false
 }
+
+}
+
+
+// return word === reversedWord;
+
+
 
 /* 
   Add your pseudocode here
+  if reverse is same as forward
+   return true. 
+  Else
+    return false
+
+  const letters = word.split('');
+  const reverseLetters = letters.reverse();
+  const reverseString  = reverseLetters.join('')
+
+  if (word === reverseString){
+    return true
+
+  } else {
+    return false
+  }
+
+}
+
 */
 
 /*
   Add written explanation of your solution here
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
